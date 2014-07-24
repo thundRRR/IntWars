45283b08967f64b070ee2bec9aaee56de0cda18b
Elyotna <raptorteak@gmail.com>
2014-07-24 15:38:55 -0700

    Removed log message

--- gamed/src/Projectile.cpp
+++ gamed/src/Projectile.cpp
@@ -19,8 +19,6 @@ void Projectile::update(int64 diff) {
             return;
          }
          
-         printf("Checking with 0x%08X (%f ; %f) !\n", it.second->getNetId(), it.second->getX(), it.second->getY());
-         
          if(collide(it.second)) {
             printf("Collide with 0x%08X !\n", it.second->getNetId());
             originSpell->applyEffects(it.second, this);
