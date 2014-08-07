1c5298a937eb5036f91fa5c1a332714e5ac5bda1
SpiritedDusty <spiriteddusty@gmail.com>
2014-08-07 14:09:26 -0700

    Reformatting & changed variables for clarity

--- lua/champions/Caitlyn/E.lua
+++ lua/champions/Caitlyn/E.lua
@@ -1,23 +1,22 @@
 Vector2 = require 'Vector2' -- include 2d vector lib 
 
 function finishCasting()
-local current = Vector2:new(getOwnerX(), getOwnerY())
-local to = Vector2:new(getSpellToX(), getSpellToY())
+    local current = Vector2:new(getOwnerX(), getOwnerY())
+    local to = Vector2:new(getSpellToX(), getSpellToY())
 
-to:sub(current)
+    to:sub(current)
+    to:normalize()
 
-to:normalize()
+    local teleportTo = to.mult(-1)
 
-local teleportTo = to.mult(-1)
-teleportTo.mult(400)
+    teleportTo.mult(400)
 
+    local trueCoords = current.add(teleportTo)
 
-local trueCoords = current.add(teleportTo)
-teleportTo(trueCoords.x, trueCoords.y)
-addProjectile(trueCoords.x, trueCoords.y)
-
+    teleportTo(trueCoords.x, trueCoords.y)
+    addProjectile(trueCoords.x, trueCoords.y)
 end
 
 function applyEffects()
---todo slow and damage enemy
+    --todo slow and damage enemy
 end
--- lua/champions/Ezreal/Q.lua
+++ lua/champions/Ezreal/Q.lua
@@ -1,21 +1,18 @@
 Vector2 = require 'Vector2' -- include 2d vector lib 
 
-
 function finishCasting()
-local current = Vector2:new(getOwnerX(), getOwnerY())
-local to = Vector2:new(getSpellToX(), getSpellToY())
+    local current = Vector2:new(getOwnerX(), getOwnerY())
+    local to = Vector2:new(getSpellToX(), getSpellToY())
 
-to:sub(current)
+    to:sub(current)
+    to:normalize()
 
-to:normalize()
-local range = Vector2:Mult(to,1150)
-local trueCoords = Vector2:Add(current, range)
+    local range = Vector2:Mult(to,1150)
+    local trueCoords = Vector2:Add(current, range)
 
-addProjectile(trueCoords.x, trueCoords.y)
-
+    addProjectile(trueCoords.x, trueCoords.y)
 end
 
 function applyEffects()
-print("Ezreal Q hit something!")
-
+    print("Ezreal Q hit something!")
 end
