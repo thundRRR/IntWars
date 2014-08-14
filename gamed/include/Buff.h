b7446fc2cf3d7400a2b5f71fdd3e4838cb3b540a
RaptorTeak <raptorteak@gmail.com>
2014-08-14 02:08:41 -0700

    Fixed compile, corrected a bit the codestyle

--- gamed/include/Buff.h
+++ gamed/include/Buff.h
@@ -1,32 +1,30 @@
-
-
 #ifndef BUFF_H
-#define	BUFF_H
+#define BUFF_H
+
+#include "stdafx.h"
 
 class Buff{
 private:
-float duration;
-float movementSpeedPercentModifier;
-float timeElapsed = 0;
-bool remove = false;
-    
+   float duration;
+   float movementSpeedPercentModifier;
+   float timeElapsed;
+   bool remove;
+
 public:
     
-    bool needsToRemove(){  return remove;}
-    Buff(float dur) : duration(dur){}
-    
-    float getMovementSpeedPercentModifier() { return movementSpeedPercentModifier;}
-    
-    void setMovementSpeedPercentModifier(float modifier){movementSpeedPercentModifier = modifier;}
-    
-    void update(uint64 diff){
-        timeElapsed += (float)diff/1000000.0;
-        if(timeElapsed >= duration){
-            remove = true;
-        }
-    }
-    
-    
+   bool needsToRemove() { return remove; }
+   Buff(float dur) : duration(dur), timeElapsed(0), remove(false) { }
+
+   float getMovementSpeedPercentModifier() { return movementSpeedPercentModifier;}
+
+   void setMovementSpeedPercentModifier(float modifier){ movementSpeedPercentModifier = modifier; }
+
+   void update(uint64 diff){
+      timeElapsed += (float)diff/1000000.0;
+      if(timeElapsed >= duration){
+         remove = true;
+      }
+   } 
 };
 
 
