1d2bec3b05504aef382218fed76e85f89cb6d9ac
Gusor <emailaddress@emailaddress.com>
2014-09-15 21:03:02 +0200

    Cleanup

--- gamed/src/Buff.cpp
+++ gamed/src/Buff.cpp
@@ -4,29 +4,21 @@
 
 
 void Buff::update(int64 diff){
-   printf("Update\n");
-  // printf("Name is %s\n", name.c_str());//this line crashes, even though name is defined and print works from constructor.. wtf
    timeElapsed += (float)diff/1000000.0;
    if(timeElapsed >= duration){
-attachedTo->getMap()->getGame()->notifyRemoveBuff(attachedTo, name);
+      if(name != ""){ // empty name = no buff icon
+         attachedTo->getMap()->getGame()->notifyRemoveBuff(attachedTo, name);
+      }
     remove = true;
 
    }
 }
 
-Buff::Buff(std::string buffName, float dur, Unit* u)  /*: duration(dur),  name(buffName), timeElapsed(0), remove(false), attachedTo(u)*/{
-duration = dur;
-name = buffName;
-timeElapsed = 0;
-remove = false;
-attachedTo = u;
-printf("New buff!\n");
-   printf("Beginning Buff name %s \n", getName().c_str());
+Buff::Buff(std::string buffName, float dur, Unit* u)  : duration(dur),  name(buffName), timeElapsed(0), remove(false), attachedTo(u){
    if(name != ""){ // empty name = no buff icon
-   attachedTo->getMap()->getGame()->notifyAddBuff(attachedTo, name);
+      attachedTo->getMap()->getGame()->notifyAddBuff(attachedTo, name);
    }
-   
-   printf("Name is %s", name.c_str());
+
 
 }
 
