7844b75c2067b279e22cc05b18b3d1c1aa0bc328
joaquin95 <joaquin95@users.noreply.github.com>
2014-09-17 18:43:52 -0300

    Evelynn's W

---
+++ lua/champions/Evelynn/W.lua
@@ -0,0 +1,10 @@
+function finishCasting()
+   print("Speed increase" ..getEffectValue(2))
+   local buff = Buff.new("", getEffectValue(3), getOwner())
+   buff:setMovementSpeedPercentModifier(getEffectValue(2)) -- It doesn't work at spell level 1, I don't know why.
+   addBuff(buff)
+end
+
+function applyEffects()
+
+end
