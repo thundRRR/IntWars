thundRRR/IntWars

commit cea7f6cf6f40c484d57ee18b8e392c49354f5a01

Give turrets basic target prioritization

gamed/src/Turret.cpp
@@ -22,17 +22,23 @@ void Turret::update(int64 diff)
    // No target : try to find a new one
    if(!isAttacking && !unitTarget) {
       const std::map<uint32, Object*>& objects = map->getObjects();
-
+      Unit* nextTarget;
+      unsigned int nextTargetPriority = 10;
       for(auto& it : objects) {
          Unit* u = dynamic_cast<Unit*>(it.second);
 
          if(!u || u->isDead() || u->getSide() == getSide() || distanceWith(u) > TURRET_RANGE) {
             continue;
          }
-
-         unitTarget = u;
-         map->getGame()->notifySetTarget(this, u);
-         break;
+         auto priority = classifyTarget(u);
+         if (priority < nextTargetPriority) {
+            nextTarget = u;
+            nextTargetPriority = priority;
+         }
+      }
+      if (nextTarget) {
+         unitTarget = nextTarget;
+         map->getGame()->notifySetTarget(this, nextTarget);
       }
    } else if(unitTarget && distanceWith(unitTarget) > TURRET_RANGE) {
       setUnitTarget(0);
@@ -40,4 +46,30 @@ void Turret::update(int64 diff)
    }
 
    Unit::update(diff);
+}
+//Prioritize targets
+unsigned int Turret::classifyTarget(Unit* u) {
+   Minion* m = dynamic_cast<Minion*>(u);
+
+   if (m) {
+      switch (m->getType()) {
+         case MINION_TYPE_MELEE:
+            return 4;
+         case MINION_TYPE_CASTER:
+            return 5;
+         case MINION_TYPE_CANNON:
+         case MINION_TYPE_SUPER:
+            return 3;
+      }
+   }
+
+   Champion* c = dynamic_cast<Champion*>(u);
+   if (c) {
+      return 6;
+   }
+
+   //Trap (Shaco box) return 1
+   //Pet (Tibbers) return 2
+
+   return 10;
 }

gamed/include/Minion.h
@@ -15,7 +15,8 @@ enum MinionSpawnPosition : uint32 {
 enum MinionSpawnType : uint8 {
    MINION_TYPE_MELEE = 0x00,
    MINION_TYPE_CASTER = 0x01,
-   MINION_TYPE_CANNON = 0x02
+   MINION_TYPE_CANNON = 0x02,
+   MINION_TYPE_SUPER = 0x03
 };
 
 class Minion : public Unit {

gamed/include/Turret.h
@@ -6,6 +6,7 @@
 class Turret : public Unit {
 private:
    std::string name;
+   unsigned int classifyTarget(Unit* u);
 
 public:
    Turret(Map* map, uint32 id, const std::string& name, float x = 0, float y = 0, float hp = 0, float ad = 0, int side = 0);
@@ -13,6 +14,7 @@ public:
    const std::string& getName() const { return name; }
    virtual void update(int64 diff) override;
    void refreshWaypoints() override {}
+   
 
 };
 

commit c7bf901acb370e0bddf5e9e3141476c8767b3487

Fix crash introduced in cea7f6c

gamed/src/Turret.cpp
@@ -22,7 +22,7 @@ void Turret::update(int64 diff)
    // No target : try to find a new one
    if(!isAttacking && !unitTarget) {
       const std::map<uint32, Object*>& objects = map->getObjects();
-      Unit* nextTarget;
+      Unit* nextTarget = 0;
       unsigned int nextTargetPriority = 10;
       for(auto& it : objects) {
          Unit* u = dynamic_cast<Unit*>(it.second);